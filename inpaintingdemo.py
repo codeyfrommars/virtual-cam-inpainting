8985dd69843d9a70dbbdcf50dce1ddd09585351c
RickHanish <rickhanish@utexas.edu>
2023-11-12 14:53:04 -0600

    Draw outline around eyes

--- inpaintingdemo.py
+++ inpaintingdemo.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import pyvirtualcam
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 
@@ -23,7 +24,36 @@ def add_eye(minX, maxX, minY, maxY, eye, im):
         )
 
 
+def add_outline(face, im):
+    img_height, img_width = im.shape[:2]
+    left_eye_indices = [33, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7]
+    right_eye_indices = [263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249]
+    left_eye_landmarks = [face[i] for i in left_eye_indices]
+    right_eye_landmarks = [face[i] for i in right_eye_indices]
+    left_eye_landmarks = [(int(landmark.x * img_width), int(landmark.y * img_height)) for landmark in left_eye_landmarks]
+    right_eye_landmarks = [(int(landmark.x * img_width), int(landmark.y * img_height)) for landmark in right_eye_landmarks]
+    left_eye_np = np.array(left_eye_landmarks)
+    right_eye_np = np.array(right_eye_landmarks)
+
+    eye_fill_mask = np.zeros_like(im, dtype=np.uint8)
+    eye_outline_mask = np.zeros_like(im, dtype=np.uint8)
+    cv2.fillPoly(eye_fill_mask, [left_eye_np], (0, 255, 0))
+    cv2.fillPoly(eye_fill_mask, [right_eye_np], (0, 255, 0))
+    cv2.polylines(eye_outline_mask, [left_eye_np], True, (0, 0, 255), 2)
+    cv2.polylines(eye_outline_mask, [right_eye_np], True, (0, 0, 255), 2)
+
+    alpha = 0.4
+    fill_mask = eye_fill_mask.astype(bool)
+    outline_mask = eye_outline_mask.astype(bool)
+    im[fill_mask] = cv2.addWeighted(im, alpha, eye_fill_mask, 1 - alpha, 0)[fill_mask]
+    im[outline_mask] = eye_outline_mask[outline_mask]
+
+    return im
+
+
 def draw(face, im):
+    # Face mesh indices:
+    # https://github.com/google/mediapipe/blob/a908d668c730da128dfa8d9f6bd25d519d006692/mediapipe/modules/face_geometry/data/canonical_face_model_uv_visualization.png
     left_eye_outer = face[33]
     left_eye_inner = face[133]
     left_eye_upper = face[159]
@@ -66,16 +96,15 @@ def create_virtual_cam():
                 # To improve performance, optionally mark the image as not writeable
                 # to pass by reference
                 image.flags.writeable = False
-                # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 results = face_mesh.process(image)
 
                 # Draw the face mesh annotations on the image
                 image.flags.writeable = True
-                # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 if results.multi_face_landmarks:
                     for face_landmarks in results.multi_face_landmarks:
                         face = face_landmarks.landmark
-                        draw(face, image)
+                        # draw(face, image)
+                        image = add_outline(face, image)
 
                         # mp_drawing.draw_landmarks(
                         #     image=image,
